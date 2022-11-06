ce939bbbb648bdedea6dbcabacc9a8eb045eb8cb
Alejandro Vozella <alejandrovozella@gmail.com>
2022-11-06 08:06:58 -0300

    Modify User

--- src/models/recepcionista.js
+++ src/models/recepcionista.js
@@ -1,26 +1,22 @@
 const mongoose = require("mongoose");
 
 const recepcionistaEsquema = new mongoose.Schema({
-    usuario: {
-        type: String,
-        require : [true,"el usuario es requerido"],
-
-    },
-    password : {
-        type: String,
-        require : [true,"la  es requerida"],
-        
-    },
-    Nombre : {
-        type: String,
-        require : [true,"el Nombre  es requerido"],
-        
-    },
-    Apellido : {
-        type: String,
-        require : [true,"el Apellido  es requerido"],
-        
-    },
+  usuario: {
+    type: String,
+    require: [true, "el usuario es requerido"],
+  },
+  password: {
+    type: String,
+    require: [true, "la  es requerida"],
+  },
+  nombre: {
+    type: String,
+    require: [true, "el Nombre  es requerido"],
+  },
+  apellido: {
+    type: String,
+    require: [true, "el Apellido  es requerido"],
+  },
 });
 
-module.exports=mongoose.model("Recepcionista",recepcionistaEsquema);
+module.exports = mongoose.model("Recepcionista", recepcionistaEsquema);
